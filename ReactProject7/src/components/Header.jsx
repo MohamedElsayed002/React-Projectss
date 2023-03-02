bb1594dac0c3dd7dbf99a87d4461b060bc6a770d
Mohamed Elsayed Ali <mohamedelsayed20258@gmail.com>
2023-03-03 01:34:16 +0200

    meme-generator

---
+++ ReactProject7/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Mohamed</h1>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header
